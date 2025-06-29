9acdbcb949b61d2d9bfdfe9d4f3c205f0085c353
Therighthon <52086285+Therighthon@users.noreply.github.com>
2025-06-29 11:53:51 -0700

    Add Marine Plant-likes
    TODO:
    - Add world generation features
    - Mussels rework
    - More starfish colors?

---
+++ src/main/java/net/dries007/tfc/common/blocks/plant/CreepingWaterPlantBlock.java
@@ -0,0 +1,106 @@
+package net.dries007.tfc.common.blocks.plant;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.PipeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.Nullable;
+
+import net.dries007.tfc.common.TFCTags;
+import net.dries007.tfc.common.blocks.ExtendedProperties;
+import net.dries007.tfc.common.fluids.FluidHelpers;
+import net.dries007.tfc.common.fluids.FluidProperty;
+import net.dries007.tfc.common.fluids.IFluidLoggable;
+import net.dries007.tfc.util.Helpers;
+import net.dries007.tfc.util.registry.RegistryPlant;
+
+public abstract class CreepingWaterPlantBlock extends CreepingPlantBlock implements IFluidLoggable
+{
+    public static CreepingWaterPlantBlock create(RegistryPlant plant, FluidProperty fluid, ExtendedProperties properties)
+    {
+        return new CreepingWaterPlantBlock(properties)
+        {
+            @Override
+            public RegistryPlant getPlant()
+            {
+                return plant;
+            }
+
+            @Override
+            public FluidProperty getFluidProperty()
+            {
+                return fluid;
+            }
+
+            @Override
+            public boolean canCreepOn(LevelReader level, BlockPos pos, BlockState state, Direction direction)
+            {
+                return Helpers.isBlock(state, TFCTags.Blocks.CREEPING_STONE_PLANTABLE_ON) && super.canCreepOn(level, pos, state, direction);
+            }
+
+        };
+    }
+
+    protected CreepingWaterPlantBlock(ExtendedProperties properties)
+    {
+        super(properties);
+
+        registerDefaultState(getStateDefinition().any().setValue(getFluidProperty(), getFluidProperty().keyFor(Fluids.EMPTY)));
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context)
+    {
+        BlockPos pos = context.getClickedPos();
+        FluidState fluidState = context.getLevel().getFluidState(pos);
+        BlockState state = defaultBlockState();
+        if (getFluidProperty().canContain(fluidState.getType()))
+        {
+            state = state.setValue(getFluidProperty(), getFluidProperty().keyFor(fluidState.getType()));
+        }
+        return updateStateFromSides(context.getLevel(), context.getClickedPos(), state);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos)
+    {
+        FluidHelpers.tickFluid(level, currentPos, state);
+        state = state.setValue(PipeBlock.PROPERTY_BY_DIRECTION.get(direction), canCreepOn(level, facingPos, facingState, direction));
+        return isEmptyContents(state) ? Blocks.AIR.defaultBlockState() : state;
+    }
+
+    private static boolean isEmptyContents(BlockState state)
+    {
+        for (BooleanProperty property : SHAPES.keySet())
+        {
+            if (state.getValue(property))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
+    {
+        super.createBlockStateDefinition(builder);
+        builder.add(getFluidProperty());
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state)
+    {
+        return IFluidLoggable.super.getFluidState(state);
+    }
+}
--- src/main/java/net/dries007/tfc/common/blocks/plant/Plant.java
+++ src/main/java/net/dries007/tfc/common/blocks/plant/Plant.java
@@ -67,6 +67,16 @@ public enum Plant implements RegistryPlant
     RADDIA_GRASS(BlockType.SHORT_GRASS, 0.9F),
     RED_OAT_GRASS(BlockType.SHORT_GRASS, 0.9F, true, -0.1f, 0.33f, 0.45f, 0.55f, 0.75f, 0.85f),
 
+    // Intertidal Plant-Likes
+    ANEMONE_GREEN(BlockType.ANENOME_CLUSTER_LIKE, 1.0F),
+    ANEMONE_PURPLE(BlockType.ANENOME_CLUSTER_LIKE, 1.0F),
+    ANEMONE_LARGE_ORANGE(BlockType.STARFISH_LIKE, 1.0F),
+    ANEMONE_LARGE_PURPLE(BlockType.STARFISH_LIKE, 1.0F),
+    BARNACLES(BlockType.BARNACLE_LIKE, 1.0F),
+    MUSSELS(BlockType.BARNACLE_LIKE, 1.0F),
+    STARFISH(BlockType.STARFISH_LIKE, 1.0F),
+    ZOSTERA(BlockType.ANENOME_CLUSTER_LIKE, 1.0F),
+
     // Other Plants
     ALLIUM(BlockType.STANDARD, 1.0F, false, -0.15F, 0.2F, 0.3F, 0.4F, 0.64F, 0.92F),
     ANTHURIUM(BlockType.STANDARD, 1.0F),
@@ -478,6 +488,9 @@ public enum Plant implements RegistryPlant
         BRANCHING_CACTUS((plant, type) -> BranchingCactusBlock.createBody(fire(solid()).strength(0.25f).sound(SoundType.WOOL).pathType(PathType.DAMAGE_OTHER))),
         BRANCHING_CACTUS_TOP((plant, type) -> GrowingBranchingCactusBlock.createGrowing(fire(solid()).randomTicks().strength(0.25f).sound(SoundType.WOOL).pathType(PathType.DAMAGE_OTHER), plant.transform(), plant.secondTransform())),
         // Water
+        BARNACLE_LIKE((plant, type) -> CreepingWaterPlantBlock.create(plant, TFCBlockStateProperties.SALT_WATER, ExtendedProperties.of(nonSolid(plant).sound(SoundType.BASALT)))),
+        ANENOME_CLUSTER_LIKE((plant, type) -> CreepingWaterPlantBlock.create(plant, TFCBlockStateProperties.SALT_WATER, ExtendedProperties.of(nonSolid(plant).sound(SoundType.BASALT)))),
+        STARFISH_LIKE((plant, type) -> RotatableWaterPlantBlock.create(plant, TFCBlockStateProperties.SALT_WATER, ExtendedProperties.of(nonSolid(plant).sound(SoundType.SLIME_BLOCK)))),
         KELP((plant, type) -> TFCKelpBlock.create(nonSolidTallPlant(plant).lootFrom(plant.transform()), plant.transform(), Direction.UP, BodyPlantBlock.THIN_BODY_SHAPE, TFCBlockStateProperties.SALT_WATER, plant)),
         KELP_TOP(((plant, type) -> TFCKelpTopBlock.create(nonSolidTallPlant(plant), plant.transform(), Direction.UP, BodyPlantBlock.TWISTING_THIN_SHAPE, TFCBlockStateProperties.SALT_WATER, plant))),
         KELP_TREE((plant, type) -> KelpTreeBlock.create(ExtendedProperties.of(kelp(plant)), TFCBlockStateProperties.SALT_WATER)),
