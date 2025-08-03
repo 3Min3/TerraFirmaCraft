1b4ee3eed6fd0abed1fdd8a0270e4fd6d4c3597c
Daniel <danielpetkau@gmail.com>
2025-08-02 21:31:45 -0700

    Fix stupid mistake v2

--- src/test/java/net/dries007/tfc/test/recipes/CraftingRecipesTest.java
+++ src/test/java/net/dries007/tfc/test/recipes/CraftingRecipesTest.java
@@ -13,6 +13,7 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -33,6 +34,7 @@ import net.neoforged.neoforge.common.Tags;
 
 public class CraftingRecipesTest implements TestSetup
 {
+    @SuppressWarnings("unchecked")
     @Test
     public void testCraftingRecipesWithToolsDamageInputs()
     {
@@ -49,25 +51,25 @@ public class CraftingRecipesTest implements TestSetup
                 if (recipe instanceof AdvancedShapedRecipe || recipe instanceof AdvancedShapelessRecipe)
                 {
                     Class<?> recipeType = recipe instanceof AdvancedShapedRecipe ? AdvancedShapedRecipe.class : AdvancedShapelessRecipe.class;
-                    @Nullable Field result = null;
+                    @Nullable Field remainder = null;
                     try
                     {
-                        result = recipeType.getDeclaredField("result");
+                        remainder = recipeType.getDeclaredField("remainder");
                     }
                     catch (Exception e)
                     {
-                        fail("Could not get field 'result' of recipeType: " + e);
+                        fail("Could not get field 'remainder' of recipeType: " + e);
                     }
 
-                    result.setAccessible(true);
-                    @Nullable ItemStackProvider resultProvider = null;
+                    remainder.setAccessible(true);
+                    @Nullable Optional<ItemStackProvider> remainderProvider = null;
                     try {
-                        resultProvider = (ItemStackProvider) result.get(recipe);
+                        remainderProvider = (Optional<ItemStackProvider>) remainder.get(recipe);
                     } catch (IllegalArgumentException | IllegalAccessException e) {
-                        fail("Could not get value of field 'result' of recipeType: " + e);
+                        fail("Could not get value of field 'remainder' of recipeType: " + e);
                     }
-
-                    if (resultProvider.modifiers().contains(DamageCraftingRemainderModifier.INSTANCE))
+                    
+                    if (remainderProvider.isPresent() && remainderProvider.get().modifiers().stream().anyMatch(modifier ->  modifier instanceof DamageCraftingRemainderModifier))
                     {
                         return false;
                     }
